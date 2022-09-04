c98981a97149b80ac8240cf5e8848c25b80ddee3
hurima90-kim <hurima90@gmail.com>
2022-09-04 23:45:26 +0900

    feat: output post data using props from main page

--- src/components/Main/PostList.tsx
+++ src/components/Main/PostList.tsx
@@ -1,24 +1,10 @@
 import React, { FunctionComponent } from 'react'
 import styled from '@emotion/styled'
 import PostItem from './PostItem'
-
-export type PostType = {
-  node: {
-    id: string
-    frontmatter: {
-      title: string
-      summary: string
-      date: string
-      categories: string[]
-      thumbnail: {
-        publicURL: string
-      }
-    }
-  }
-}
+import { PostListItemType } from 'types/PostItem.types'
 
 type PostListProps = {
-  posts: PostType[]
+  posts: PostListItemType[]
 }
 
 const PostListWrapper = styled.div`
@@ -38,7 +24,7 @@ const PostListWrapper = styled.div`
 const PostList: FunctionComponent<PostListProps> = function ({ posts }) {
   return (
     <PostListWrapper>
-      {posts.map(({ node: { id, frontmatter } }: PostType) => (
+      {posts.map(({ node: { id, frontmatter } }: PostListItemType) => (
         <PostItem {...frontmatter} link="https://www.google.com" key={id} />
       ))}
     </PostListWrapper>
