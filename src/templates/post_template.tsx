982beeebffeabc20e93a37f2935540a2400b0fdd
hurima90-kim <hurima90@gmail.com>
2022-09-13 22:57:00 +0900

    feat: ouput of a post in markdown format

--- src/templates/post_template.tsx
+++ src/templates/post_template.tsx
@@ -3,6 +3,7 @@ import { graphql } from 'gatsby'
 import { PostFrontmatterType, PostPageItemType } from 'types/PostItem.types'
 import Template from 'components/Common/Template'
 import PostHead from 'components/Post/PostHead'
+import PostContent from 'components/Post/PostContent'
 
 type PostTemplateProps = {
   data: {
@@ -47,6 +48,7 @@ const PostTemplate: FunctionComponent<PostTemplateProps> = function ({
         categories={categories}
         thumbnail={gatsbyImageData}
       />
+      <PostContent html={html} />
     </Template>
   )
 }
