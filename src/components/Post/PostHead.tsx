d4b767e5b7c4839f8bec60ff99bc95d8172d46a6
hurima90-kim <hurima90@gmail.com>
2022-09-17 00:26:20 +0900

    style: add responsive css code for 768px

--- src/components/Post/PostHead.tsx
+++ src/components/Post/PostHead.tsx
@@ -17,6 +17,10 @@ const PostHeadWrapper = styled.div`
   position: relative;
   width: 100%;
   height: 400px;
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 // !important 속성이 없으면 스타일 적용 순위에서 밀리기 때문에 위의 코드와 같이 직접 인라인으로 포지션 스타일을 넘겨주었습니다.(!important 속성은 가능한 사용하지 말아야 하는 속성)
 const BackgroundImage = styled((props: GatsbyImgProps) => (
@@ -27,6 +31,10 @@ const BackgroundImage = styled((props: GatsbyImgProps) => (
   height: 400px;
   object-fit: cover;
   filter: brightness(0.25);
+
+  @media (max-width: 768px) {
+    height: 300px;
+  }
 `
 
 const PostHead: FunctionComponent<PostHeadProps> = function ({
